4629248bbca38592383ddf951e326f6678dc0617
thosvesta <javid24hussain@gmail.com>
2017-07-26 10:29:08 +0530

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -9,10 +9,10 @@ if ('serviceWorker' in navigator) {
 			//console.log('ServiceWorker registration succeeded, with this scope: ', registration.scope);
 			// you may occasionally need to clear a service worker; this is the only way i've found to do that...
 			// comment this out while not using it
-			registration.unregister().then(function(boolean) {
+			/*registration.unregister().then(function(boolean) {
 				// if boolean = true, unregister is successful
 				console.log('ServiceWorker unregistered');
-			});
+			});*/
 		}).catch(function(err) {
 			// registration failed :-(
 			console.log('ServiceWorker registration failed: ', err);
