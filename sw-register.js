167dcb919983ee915b51680ccd104c6097732f15
thosvesta <javid24hussain@gmail.com>
2017-07-26 10:12:34 +0530

    Update sw-register.js

--- sw-register.js
+++ sw-register.js
@@ -1,4 +1,6 @@
 // check if Service Worker is supported
+// <script type='text/javascript' src='/service-worker-register.js'></script>
+
 if ('serviceWorker' in navigator) {
 	// register the Service Worker, must be in the root directory to have site-wide scope...
 	navigator.serviceWorker.register('/serviceworker-min.js')
@@ -7,10 +9,10 @@ if ('serviceWorker' in navigator) {
 			//console.log('ServiceWorker registration succeeded, with this scope: ', registration.scope);
 			// you may occasionally need to clear a service worker; this is the only way i've found to do that...
 			// comment this out while not using it
-			/*registration.unregister().then(function(boolean) {
+			registration.unregister().then(function(boolean) {
 				// if boolean = true, unregister is successful
 				console.log('ServiceWorker unregistered');
-			});*/
+			});
 		}).catch(function(err) {
 			// registration failed :-(
 			console.log('ServiceWorker registration failed: ', err);
